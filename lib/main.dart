2d21026801994b37f948d03e21db8b093fcaa542
younha00 <ayeon0@office.skhu.ac.kr>
2024-04-30 16:11:30 +0900

    배포 테스트2

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Secret Forest!',
+      title: 'Secret Forest',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
