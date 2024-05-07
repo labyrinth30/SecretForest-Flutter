59f8f3a040586c49bd5ed8c8149c1c27126a78f4
younha00 <ayeon0@office.skhu.ac.kr>
2024-05-07 13:05:38 +0900

    ✨ [#5]Feat: dioProvider 추가

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:secret_forest_flutter/riverpod/auth_store.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+    WidgetRef ref,
+  ) {
+    final authState = ref.watch(authProvider);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Secret Forest'),
+        title: Text('비밀의숲 ${authState.email}'),
       ),
       body: Center(
         child: ElevatedButton(
