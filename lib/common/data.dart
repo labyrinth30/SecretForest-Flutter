f94215f4578955c8a1e7c708e7a12a44f85f79c3
younha00 <ayeon0@office.skhu.ac.kr>
2024-06-19 02:34:51 +0900

    ✨ Feat: ip 수정

--- lib/common/data.dart
+++ lib/common/data.dart
@@ -1,3 +1,6 @@
-const authIp = 'http://localhost:3001';
-const reservationIp = 'http://localhost:3000';
-const commonUrl = 'http://localhost:61180';
+const authIp =
+    'http://http://k8s-default-secretfo-f50dd79e90-1614778431.ap-northeast-2.elb.amazonaws.com';
+const reservationIp =
+    'http://http://k8s-default-secretfo-f50dd79e90-1614778431.ap-northeast-2.elb.amazonaws.com';
+const commonUrl =
+    'http://http://k8s-default-secretfo-f50dd79e90-1614778431.ap-northeast-2.elb.amazonaws.com';
